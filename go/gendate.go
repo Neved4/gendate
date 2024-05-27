6a05183555815d80a4811d9b8b55129d39e12c00
Neveda <63655535+Neved4@users.noreply.github.com>
2024-05-27 23:27:51 +0200

    Remove unnecessary `isLeapYear()` since `time` accounts for leap years

--- go/gendate.go
+++ go/gendate.go
@@ -5,12 +5,8 @@ import (
 	"time"
 )
 
-func isLeapYear(year int) bool {
-	return (year%4 == 0 && year%100 != 0) || (year%400 == 0)
-}
-
 func main() {
-	isEnd, isLeap := false, false
+	isEnd := false
 	currentDate := time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
 
 	fmt.Println(currentDate.Format("2006-01-02"))
@@ -18,19 +14,11 @@ func main() {
 	for {
 		currentDate = currentDate.AddDate(0, 0, 1)
 
-		isLeap = (!isLeapYear(currentDate.Year()) &&
-			currentDate.Month() == 2 &&
-			currentDate.Day() == 29)
-
-		if isLeap {
-			continue
-		}
-
 		fmt.Println(currentDate.Format("2006-01-02"))
 
-		isEnd = (currentDate.Year() == 3000 &&
+		isEnd = currentDate.Year() == 3000 &&
 			currentDate.Month() == 1 &&
-			currentDate.Day() == 1)
+			currentDate.Day() == 1
 
 		if isEnd {
 			break
