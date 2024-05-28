b4604ca0fbeda110e7a3e4bcdfb8da5e5e59fd47
Neveda <63655535+Neved4@users.noreply.github.com>
2024-05-28 12:33:05 +0200

    Simplify Haskell code further

--- hs/gendate.hs
+++ hs/gendate.hs
@@ -1,28 +1,11 @@
 #!/usr/bin/env runhaskell
 
 import Data.Time
-import Data.Time.Format (defaultTimeLocale, formatTime)
-
-dateToString :: Day -> String
-dateToString date = formatTime defaultTimeLocale "%Y-%m-%d" date
-
-incrementDate :: Day -> Day
-incrementDate = addDays 1
-
-mainLoop :: Day -> IO ()
-mainLoop currentDate = do
-  let dateStr = dateToString currentDate
-
-  putStrLn dateStr
-
-  let (year, month, day) = toGregorian currentDate
-      isEnd = year == 3000 && month == 1 && day == 1
-
-  if not isEnd
-    then mainLoop (incrementDate currentDate)
-    else return ()
 
 main :: IO ()
 main = do
   let startDate = fromGregorian 1970 1 1
-  mainLoop startDate
+      endDate = fromGregorian 3000 1 1
+      dateRange = [startDate .. endDate]
+      dateStrings = map (formatTime defaultTimeLocale "%Y-%m-%d") dateRange
+  mapM_ putStrLn dateStrings
