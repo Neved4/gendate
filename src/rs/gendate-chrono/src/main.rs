086e0003377a932af441fd453299c6e14ca37fa5
Neveda <63655535+Neved4@users.noreply.github.com>
2024-06-11 17:39:43 +0200

    Rename Rust projects

---
+++ src/rs/gendate-chrono/src/main.rs
@@ -0,0 +1,22 @@
+use chrono::{ Duration, NaiveDate };
+use std::io::{ self, Write };
+
+fn main() {
+	let stdout: io::StdoutLock<'_> = io::stdout().lock();
+	let mut handle: io::BufWriter<_> = io::BufWriter::new(stdout);
+
+	let start_date: NaiveDate = NaiveDate::from_ymd_opt(1970, 1, 1)
+		.expect("Invalid start date");
+	let end_date: NaiveDate = NaiveDate::from_ymd_opt(3000, 1, 1)
+		.expect("Invalid end date");
+
+	let mut current_date: NaiveDate = start_date;
+
+	while current_date <= end_date {
+		writeln!(handle, "{}", current_date.format("%Y-%m-%d"))
+			.expect("Failed to writeln");
+		current_date += Duration::days(1);
+	}
+
+	handle.flush().expect("Unable to flush stdout");
+}
